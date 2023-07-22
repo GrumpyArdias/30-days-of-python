d36a5319f25f2bfe0cf4e9c909246d529c28c1d2
GrumpyArdias <this.mario@gmail.com>
2023-07-22 12:15:47 +0200

    day 1

---
+++ day1/HelloWorld.py
@@ -0,0 +1,37 @@
+import math
+
+# Exercise 1 + 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3 % 4)
+print(3//4)
+
+
+print('Mario')
+print('Herrero')
+print('Spain')
+print('I am enjoying 30 days of python')
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Mario'))
+print(type('Herrero'))
+print(type('Spain'))
+
+# Exercise 3
+
+p1 = 2
+p2 = 3
+q1 = 10
+q2 = 8
+
+eucldian_disntace = math.sqrt((p1-q1)**2 + (p2-q2)**2)
+
+print("the eucldian distance is", eucldian_disntace)
